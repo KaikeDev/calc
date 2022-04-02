a4bffa437eda7b9dabf79e18c3503624227de6dc
KaikeDev <kaikevttuerpe@gmail.com>
2022-04-02 15:57:50 -0300

    finished calc

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const equation = document.querySelector('#calc_display_equation');
+const equation = document.querySelector("#calc_display_equation");
+const result = document.querySelector("#calc_display_result");
+const btnNumbers = document.querySelectorAll(".btn_number");
+const cleanBotton = document.querySelector(".calc_keyboard_btn");
+const btnOperations = document.querySelectorAll(".btn_button");
+const btnEqual = document.querySelector(".btn_Equal");
 
-const btnNumbers = document.querySelectorAll('.btn_number');
+btnNumbers.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    equation.textContent += btn.textContent;
+  });
+});
 
+cleanBotton.onclick = () => (
+  (equation.textContent = 0), (result.textContent = 0)
+);
+
+btnOperations.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    equation.textContent += btn.textContent;
+  });
+});
+
+btnEqual.onclick = () => (result.textContent = eval(equation.textContent));
